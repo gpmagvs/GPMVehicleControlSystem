64f13b2c4df9d93f9376434f863d9a5480a75c8c
jinweihuang <jinweihuang2033@outlook.com>
2023-09-19 11:46:10 +0800

    BatteryController 新增 API:RechargeSwitch , ChargeCicuitState

--- GPMVehicleControlSystem/Controllers/AGVInternal/BatteryController.cs
+++ GPMVehicleControlSystem/Controllers/AGVInternal/BatteryController.cs
@@ -1,4 +1,5 @@
-﻿using GPMVehicleControlSystem.ViewModels.BatteryQuery;
+﻿using GPMVehicleControlSystem.Models;
+using GPMVehicleControlSystem.ViewModels.BatteryQuery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,25 @@ namespace GPMVehicleControlSystem.Controllers.AGVInternal
             var results = await queryer.Query();
             results = results.OrderBy(p => p.Key).ToDictionary(p => p.Key, p => p.Value);
             return Ok(results);
+        }
+
+        /// <summary>
+        /// 控制充電迴路
+        /// </summary>
+        /// <param name="enabled"></param>
+        /// <returns></returns>
+        [HttpGet("RechargeSwitch")]
+        public async Task<IActionResult> RechargeSwitch(bool enabled)
+        {
+            bool result = await StaStored.CurrentVechicle.WagoDO.SetState(VehicleControl.DIOModule.clsDOModule.DO_ITEM.Recharge_Circuit, enabled);
+            return Ok(result);
+        }
+
+        [HttpGet("ChargeCicuitState")]
+        public async Task<IActionResult> ChargeCicuitState()
+        {
+            bool result = StaStored.CurrentVechicle.WagoDO.GetState(VehicleControl.DIOModule.clsDOModule.DO_ITEM.Recharge_Circuit);
+            return Ok(result);
         }
     }
 }
