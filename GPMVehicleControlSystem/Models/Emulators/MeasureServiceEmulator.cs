9bc7dc50d3aca706a93cc6d344d3d58b83aa7e99
jinweihuang <jinweihuang2033@outlook.com>
2023-09-22 11:42:55 +0800

    add MeasureServiceEmulator.cs TaskExtension.cs

---
+++ GPMVehicleControlSystem/Models/Emulators/MeasureServiceEmulator.cs
@@ -0,0 +1,87 @@
+﻿using AGVSystemCommonNet6.GPMRosMessageNet.Services;
+using AGVSystemCommonNet6.Log;
+using Polly.Caching;
+using RosSharp.RosBridgeClient;
+using static GPMVehicleControlSystem.Models.VehicleControl.Vehicles.TsmcMiniAGV;
+
+namespace GPMVehicleControlSystem.Models.Emulators
+{
+    public class MeasureServiceEmulator : AGVROSEmulator
+    {
+        public MeasureServiceEmulator() : base()
+        {
+        }
+
+        internal override void TopicsPublish()
+        {
+        }
+        internal override void AdvertiseServices()
+        {
+            rosSocket.AdvertiseService<VerticalCommandRequest, VerticalCommandResponse>("/command_action", CommandActionCallback);
+            LOG.INFO($"Measure Service Advertise Now!   (Service Name : /command_action)");
+
+        }
+
+        internal override void TopicsAdvertise()
+        {
+        }
+        internal override void InitNewTaskCommandActionServer()
+        {
+        }
+        private bool CommandActionCallback(VerticalCommandRequest tin, out VerticalCommandResponse tout)
+        {
+            bool confirm = false;
+            LOG.WARN($"[Measure Emu] Recieve service request : {tin.command}");
+            if (tin.command.Contains("pose"))
+            {
+                try
+                {
+                    string[] splited = tin.command.Split(',');
+                    string time_date = splited[1]; //yyyyMMdd
+                    string time_time = splited[2]; //HHmmss
+                    Task.Factory.StartNew(async () =>
+                    {
+                        await Task.Delay(5000);
+                        clsMeasureResult result = new clsMeasureResult(0)
+                        {
+                            result = "done",
+                            location = "AAA123",
+                            illuminance = 23,
+                            decibel = 44,
+                            temperature = 23.23,
+                            humudity = 55.23,
+                            IPA = 10,
+                            TVOC = 12.4,
+                            Acetone = 1,
+                            time = $"{DateTime.Now.ToString("yyyyMMdd HHmmss")}",
+                            partical_03um = 3,
+                            partical_05um = 5,
+                            partical_10um = 10,
+                            partical_30um = 30,
+                            partical_50um = 50,
+                            partical_100um = 100,
+                            PID = 2
+                        };
+                        rosSocket.CallServiceAndWait<VerticalCommandRequest, VerticalCommandResponse>("/done_action", new VerticalCommandRequest
+                        {
+                            command = result.GetCommandStr(),
+                        }); ;
+                    });
+                    confirm = true;
+                }
+                catch (Exception ex)
+                {
+                    LOG.ERROR($"command parse fail.({tin.command}){ex.Message}", ex);
+                    confirm = false;
+                }
+            }
+
+            tout = new VerticalCommandResponse
+            {
+                confirm = confirm,
+            };
+            return true;
+        }
+
+    }
+}
---
+++ GPMVehicleControlSystem/Models/VehicleControl/TaskExecute/TaskExtension.cs
@@ -0,0 +1,37 @@
+﻿using AGVSystemCommonNet6;
+using AGVSystemCommonNet6.AGVDispatch.Messages;
+using Newtonsoft.Json;
+
+namespace GPMVehicleControlSystem.Models.VehicleControl.TaskExecute
+{
+    public static class TaskExtension
+    {
+        /// <summary>
+        /// 取得任務切片
+        /// </summary>
+        /// <param name="task_download"></param>
+        /// <param name="point_int"></param>
+        /// <returns></returns>
+        public static clsTaskDownloadData Splice(this clsTaskDownloadData task_download, int start_index, int length,bool chaged_destination)
+        {
+            clsMapPoint[] to_splice_Ori_traj = task_download.ExecutingTrajecory;
+
+            clsTaskDownloadData new_data = JsonConvert.DeserializeObject<clsTaskDownloadData>(task_download.ToJson()); //深拷貝
+            if (new_data.Action_Type == ACTION_TYPE.None)
+            {
+                new_data.Trajectory = new clsMapPoint[length];
+                Array.Copy(to_splice_Ori_traj, start_index, new_data.Trajectory, 0, length);
+            }
+            else
+            {
+                new_data.Homing_Trajectory = new clsMapPoint[length];
+                Array.Copy(to_splice_Ori_traj, start_index, new_data.Homing_Trajectory, 0, length);
+            }
+            if (chaged_destination)
+            {
+                new_data.Destination = new_data.ExecutingTrajecory.Last().Point_ID;
+            }
+            return new_data;
+        }
+    }
+}
