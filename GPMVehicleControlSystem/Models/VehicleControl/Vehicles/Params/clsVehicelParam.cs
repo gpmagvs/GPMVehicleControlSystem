a2f0a0d0c19cc2121cbb79b95b1713f057620193
gpm_inspection_agvs_pc <gpm@gpmcorp.com.tw>
2023-10-11 12:47:28 +0800

    修改換電池 lock/unlock時機 可設定換電趴數

--- GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsVehicelParam.cs
+++ GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsVehicelParam.cs
@@ -114,5 +114,9 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.Vehicles.Params
     public class clsInspectionAGVParams
     {
         public bool CheckBatteryLockStateWhenInit { get; set; } = false;
+        /// <summary>
+        /// 低於此電量不換電池
+        /// </summary>
+        public byte ExchangeBatLevelThresholdVal { get; set; } = 100;
     }
 }
