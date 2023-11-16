3aa821722baec0496c21b73ec13d6186abae5ffa
jinweihuang <jinweihuang2033@outlook.com>
2023-11-16 11:50:46 +0800

    前方障礙物二次檢參數新增 -AlarmLevelWhenTrigger -Detection_Method

--- GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsObstacleDetection.cs
+++ GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsObstacleDetection.cs
@@ -1,12 +1,24 @@
-﻿namespace GPMVehicleControlSystem.Models.VehicleControl.Vehicles.Params
+﻿using AGVSystemCommonNet6.Alarm;
+
+namespace GPMVehicleControlSystem.Models.VehicleControl.Vehicles.Params
 {
     public class clsObstacleDetection
     {
+
+        public enum FRONTEND_OBS_DETECTION_METHOD
+        {
+            BEGIN_ACTION,
+            CONTINUE,
+        }
+
         public bool Enable_Load { get; set; } = false;
         public bool Enable_UnLoad { get; set; } = false;
         /// <summary>
         /// 偵測秒數
         /// </summary>
         public int Duration { get; set; } = 4;
+
+        public ALARM_LEVEL AlarmLevelWhenTrigger { get; set; } = ALARM_LEVEL.WARNING;
+        public FRONTEND_OBS_DETECTION_METHOD Detection_Method { get; set; } = FRONTEND_OBS_DETECTION_METHOD.BEGIN_ACTION;
     }
 }
