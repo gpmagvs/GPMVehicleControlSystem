8b42752099ccb815ea06c3c1433248347a15a0b7
jinweihuang <jinweihuang2033@outlook.com>
2023-12-13 08:57:41 +0800

    新增電池參數 - ChargeWhenLevelLowerThanThreshold - ChargeLevelThreshold

--- GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsBatteryParam.cs
+++ GPMVehicleControlSystem/Models/VehicleControl/Vehicles/Params/clsBatteryParam.cs
@@ -4,12 +4,17 @@
     {
         public string BatteryLogFolder { get; set; } = "C:\\Users\\jinwei\\Documents\\GPM LOG";
         public bool Recharge_Circuit_Auto_Control_In_ManualMode { get; set; } = true;
+        public bool ChargeWhenLevelLowerThanThreshold { get; set; } = true;
+
+        /// <summary>
+        /// 若ChargeWhenLevelLowerThanThreshold為true,電量低於此閥值才需開啟充電迴路
+        /// </summary>
+        public double ChargeLevelThreshold { get; set; } = 30;
         /// <summary>
         /// 切斷充電迴路的電壓閥值,當電壓大於此數值，將會切斷充電迴路，避免電池過度充電。
         /// 單位: mV
         /// </summary>
         public int CutOffChargeRelayVoltageThreshodlval { get; set; } = 28800;
-
 
     }
 }
