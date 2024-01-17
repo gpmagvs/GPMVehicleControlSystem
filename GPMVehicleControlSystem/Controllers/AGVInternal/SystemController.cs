b5a4da717170108bfa2ec7a086b9b86712bf6d0d
jinweihuang <jinweihuang2033@outlook.com>
2024-01-17 11:46:50 +0800

    新增系統重啟/關閉API與實作

--- GPMVehicleControlSystem/Controllers/AGVInternal/SystemController.cs
+++ GPMVehicleControlSystem/Controllers/AGVInternal/SystemController.cs
@@ -1,8 +1,10 @@
-﻿using GPMVehicleControlSystem.Models;
+﻿using AGVSystemCommonNet6.Log;
+using GPMVehicleControlSystem.Models;
 using GPMVehicleControlSystem.Models.VehicleControl.Vehicles;
 using GPMVehicleControlSystem.Models.VehicleControl.Vehicles.Params;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace GPMVehicleControlSystem.Controllers.AGVInternal
 {
@@ -30,5 +32,28 @@ namespace GPMVehicleControlSystem.Controllers.AGVInternal
                 return Ok(false);
             }
         }
+
+        [HttpPost("CloseSystem")]
+        public async Task<IActionResult> CloseSystem()
+        {
+
+            if (agv.Sub_Status == AGVSystemCommonNet6.clsEnums.SUB_STATUS.RUN || agv.Sub_Status == AGVSystemCommonNet6.clsEnums.SUB_STATUS.Initializing)
+                return Ok(new { confirm = false, message = $"AGV當前狀態({agv.Sub_Status})禁止重新啟動系統!" });
+            StaSysControl.SystemClose();
+            return Ok(new { confirm = true, message = "" });
+        }
+
+
+        [HttpPost("RestartSystem")]
+        public async Task<IActionResult> RestartSystem()
+        {
+            if (agv.Sub_Status == AGVSystemCommonNet6.clsEnums.SUB_STATUS.RUN|| agv.Sub_Status == AGVSystemCommonNet6.clsEnums.SUB_STATUS.Initializing)
+                return Ok(new { confirm = false, message = $"AGV當前狀態({agv.Sub_Status})禁止重新啟動系統!" });
+            StaSysControl.SystemRestart();
+            return Ok(new { confirm = true, message = "" });
+        }
+
+      
+        private Vehicle agv => StaStored.CurrentVechicle;
     }
 }
---
+++ GPMVehicleControlSystem/Models/StaSysControl.cs
@@ -0,0 +1,35 @@
+﻿using AGVSystemCommonNet6.Log;
+using System.Diagnostics;
+
+namespace GPMVehicleControlSystem.Models
+{
+    public static class StaSysControl
+    {
+
+        public static void SystemClose()
+        {
+            _ = Task.Factory.StartNew(async () =>
+            {
+                LOG.WARN("System will close after 1 sec...");
+                await Task.Delay(1000);
+                Environment.Exit(0);
+            });
+        }
+        public static void SystemRestart()
+        {
+            _ = Task.Factory.StartNew(async () =>
+            {
+                LOG.WARN($"System will restart after 1 sec...(exe:{Environment.ProcessPath})");
+                await Task.Delay(1000);
+                Process _p = new Process();
+                _p.StartInfo = new ProcessStartInfo
+                {
+                    FileName = Environment.ProcessPath,
+                    WorkingDirectory = Path.GetDirectoryName(Environment.ProcessPath)
+                };
+                _p.Start();
+            });
+        }
+
+    }
+}
