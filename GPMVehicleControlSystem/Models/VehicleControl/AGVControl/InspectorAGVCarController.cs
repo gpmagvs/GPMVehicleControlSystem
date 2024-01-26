b61c190acfdebff580a12da84011b3098a3d548d
jinweihuang <jinweihuang2033@outlook.com>
2024-01-26 09:38:30 +0800

    InspectorAGVCarController code refatory

--- GPMVehicleControlSystem/Models/VehicleControl/AGVControl/InspectorAGVCarController.cs
+++ GPMVehicleControlSystem/Models/VehicleControl/AGVControl/InspectorAGVCarController.cs
@@ -14,6 +14,9 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
         }
         public Action<clsMeasureDone> OnInstrumentMeasureDone;
         private COMMANDS action_command;
+        private DateTime previousStartMeasureTime = DateTime.MinValue;
+
+
         public InspectorAGVCarController()
         {
         }
@@ -58,9 +61,9 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
         /// <returns></returns>
         public async Task<(bool confirm, string message)> MeasurementInit()
         {
-            return await CallCommandAction(COMMANDS.init);
+            return await CallCommandAction("/command_action",COMMANDS.init);
         }
-        private DateTime previousStartMeasureTime = DateTime.MinValue;
+       
         /// <summary>
         /// 開始量測
         /// </summary>
@@ -68,8 +71,21 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
         public async Task<(bool confirm, string message)> StartInstrumentMeasure(int tagID)
         {
             previousStartMeasureTime = DateTime.Now;
-            return await CallCommandAction(COMMANDS.pose, tagID);
+            return await CallCommandAction("/command_action", COMMANDS.pose, tagID);
         }
+
+        public override Task<(bool request_success, bool action_done)> TriggerCSTReader()
+        {
+            throw new NotImplementedException("巡檢AGV不具有CST Reader 功能");
+        }
+
+        public override Task<(bool request_success, bool action_done)> AbortCSTReader()
+        {
+            throw new NotImplementedException("巡檢AGV不具有CST Reader 功能");
+        }
+
+
+        #region Private Methods
 
         /// <summary>
         /// 儀器量測結束的回調函數
@@ -93,21 +109,11 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
             return true;
         }
 
-        public override Task<(bool request_success, bool action_done)> TriggerCSTReader()
-        {
-            throw new NotImplementedException("巡檢AGV不具有CST Reader 功能");
-        }
-
-        public override Task<(bool request_success, bool action_done)> AbortCSTReader()
-        {
-            throw new NotImplementedException("巡檢AGV不具有CST Reader 功能");
-        }
-
-        private async Task<(bool confirm, string message)> CallCommandAction(COMMANDS command)
+        private async Task<(bool confirm, string message)> CallCommandAction(string service_name, COMMANDS command)
         {
             try
             {
-                VerticalCommandResponse? response = await rosSocket.CallServiceAndWait<VerticalCommandRequest, VerticalCommandResponse>("/command_action",
+                VerticalCommandResponse? response = await rosSocket.CallServiceAndWait<VerticalCommandRequest, VerticalCommandResponse>(service_name,
                         new VerticalCommandRequest
                         {
                             command = command.ToString(),
@@ -124,13 +130,12 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
             }
         }
 
-
-        private async Task<(bool confirm, string message)> CallCommandAction(COMMANDS command, int tagID)
+        private async Task<(bool confirm, string message)> CallCommandAction(string service_name, COMMANDS command, int tagID)
         {
             action_command = command;
             try
             {
-                VerticalCommandResponse? response = await rosSocket.CallServiceAndWait<VerticalCommandRequest, VerticalCommandResponse>("/command_action",
+                VerticalCommandResponse? response = await rosSocket.CallServiceAndWait<VerticalCommandRequest, VerticalCommandResponse>(service_name,
                         new VerticalCommandRequest
                         {
                             command = $"{command.ToString()},{DateTime.Now.ToString("yyyyMMdd")},{DateTime.Now.ToString("HHmmss")}",
@@ -146,6 +151,7 @@ namespace GPMVehicleControlSystem.Models.VehicleControl.AGVControl
             }
         }
 
+        #endregion
 
     }
 }
