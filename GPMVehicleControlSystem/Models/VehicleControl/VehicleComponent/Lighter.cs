fd46b6da5acbb43882f71019d239bd5991bb34ea
jinweihuang <jinweihuang2033@outlook.com>
2024-01-30 11:33:55 +0800

    Lighter Flash 方法優化

--- GPMVehicleControlSystem/Models/VehicleControl/VehicleComponent/Lighter.cs
+++ GPMVehicleControlSystem/Models/VehicleControl/VehicleComponent/Lighter.cs
@@ -26,39 +26,15 @@ namespace AGVSystemCommonNet6.Abstracts
 
         public void Flash(DO_ITEM light_DO, int flash_period = 400)
         {
-            Task.Factory.StartNew(async () =>
-            {
-                await DOModule.SetState(light_DO, true);
-                await Task.Delay(100);
-                flash_cts = new CancellationTokenSource();
-                AllCloseFlag = false;
-                bool light_active = false;
-                while (true)
-                {
-                    try
-                    {
-                        if (AllCloseFlag)
-                            break;
-                        await DOModule.SetState(light_DO, light_active);
-                        await Task.Delay(flash_period, flash_cts.Token);
-                        light_active = !light_active;
-                    }
-                    catch (Exception ex)
-                    {
-                        LOG.ERROR(ex);
-                    }
-                }
-            });
+            Flash(new DO_ITEM[] { light_DO }, flash_period);
         }
 
         public async void Flash(DO_ITEM[] light_DOs, int flash_period = 400)
         {
-            foreach (var item in light_DOs)
-                await this.DOModule.SetState(item, true);
-
-            flash_cts = new CancellationTokenSource();
-            _ = Task.Factory.StartNew(async () =>
+            Thread _flash_thred = new Thread(async () =>
             {
+                foreach (var item in light_DOs)
+                    await DOModule.SetState(item, true);
                 await Task.Delay(100);
                 AllCloseFlag = false;
                 bool light_active = false;
@@ -79,6 +55,11 @@ namespace AGVSystemCommonNet6.Abstracts
                     }
                 }
             });
+
+            flash_cts = new CancellationTokenSource();
+            _flash_thred.IsBackground = false;
+            _flash_thred.Start();
+
         }
 
 
