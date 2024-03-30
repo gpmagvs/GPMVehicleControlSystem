44e09bfb466be6a945c20897f989f716637f51fa
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-03-30 11:01:54 +0800

    新增IMU控制器

---
+++ GPMVehicleControlSystem/Controllers/AGVInternal/IMUController.cs
@@ -0,0 +1,18 @@
+﻿using GPMVehicleControlSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPMVehicleControlSystem.Controllers.AGVInternal
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IMUController : ControllerBase
+    {
+        [HttpGet("ResetMAXMINRecord")]
+        public async Task<IActionResult> ResetMAXMINRecord()
+        {
+            StaStored.CurrentVechicle.IMU.ResetMaxAndMinGvalRecord();
+            return Ok();
+        }
+    }
+}
