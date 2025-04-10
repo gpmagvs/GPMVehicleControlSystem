ae8105d285f71e8eff46d7275c84f15e42796ca9
jinweihuang20 <jinweihuang1020@gmail.com>
2025-04-10 10:33:35 +0800

    更新 restart_agvc.sh

--- GPMVehicleControlSystem/restart_agvc.sh
+++ GPMVehicleControlSystem/restart_agvc.sh
@@ -2,11 +2,11 @@
 
 echo "Restart gpm.launch ..."
 pkill -f "roslaunch agv_control_settings gpm.launch"
-sleep 2
+sleep 1
 
 source /opt/ros/kinetic/setup.bash
 source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 echo 12345678 | gnome-terminal "agvc" --window -e "sleep 1"\
-  --tab -e 'bash -c "sleep 6; roslaunch agv_control_settings gpm.launch ;exec bash"'\
+  --tab -e 'bash -c "sleep 1; roslaunch agv_control_settings gpm.launch ;exec bash"'\
   
